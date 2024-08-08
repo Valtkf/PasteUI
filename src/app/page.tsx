44b20576845f30c8105646a056834ca6691d05d5
Valtkf <valentin.vgregorio@gmail.com>
2024-08-08 16:37:50 +0200

    💄 F0 : Add styling for navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
 import Image from "next/image";
+import logopaste from "@/assets/logo/logopaste.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-semibold text-2xl">Paste UI</h1>
+    <main className="flex min-h-screen p-4 bg-[#292929] ">
+      <div className="border-2 border-[#1D1D1D] w-full min-h-screen rounded-3xl bg-[#121212]">
+        <div className="flex border-2 border-green-500 h-[85px] justify-start items-center">
+          <div className=" w-[96px]  ">
+            <Image src={logopaste} alt="" />
+          </div>
+          <div className="">
+            <li className="flex items-center space-x-8 border-x-[1px] border-stone-400 pt-4 pb-4 pl-8 pr-8 h-[24px]">
+              <a
+                href="#"
+                className="text-stone-400 hover:text-white font-normal text-sm"
+              >
+                Library
+              </a>
+              <a
+                href="#"
+                className="text-stone-400 hover:text-white font-normal text-sm"
+              >
+                Extensions
+              </a>
+              <a
+                href="#"
+                className="text-stone-400 hover:text-white font-normal text-sm"
+              >
+                Community
+              </a>
+              <a
+                href="#"
+                className="text-stone-400 hover:text-white font-normal text-sm"
+              >
+                Membership
+              </a>
+            </li>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
