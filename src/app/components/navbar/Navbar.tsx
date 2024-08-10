8f27da4dfd669dbd17063969830cda3f8d4ad692
Valtkf <valentin.vgregorio@gmail.com>
2024-08-11 01:30:15 +0200

    ✨  F0 : shadcn ui

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -5,13 +5,13 @@ import { FiUser } from "react-icons/fi";
 
 export default function Navbar() {
   return (
-    <div className="border-2 border-[#1D1D1D] w-full min-h-screen rounded-3xl bg-[#121212]">
+    <div className="rounded-t-3xl bg-[#121212]">
       <div className="flex h-[85px] justify-start items-center">
         <div className=" w-[96px]  ">
           <Image src={logopaste} alt="" />
         </div>
         <div className="">
-          <li className="flex items-center space-x-8 border-x-[1px] border-stone-400 pt-4 pb-4 pl-8 pr-8 h-[24px]">
+          <li className="flex items-center space-x-8 border-x-[1px] border-[#1D1D1D] pt-4 pb-4 pl-8 pr-8 h-[24px]">
             <a
               href="#"
               className="text-stone-400 hover:text-white font-normal text-sm"
@@ -38,16 +38,20 @@ export default function Navbar() {
             </a>
           </li>
         </div>
-        <div className="border-r-[1px] border-stone-400 pr-8">
-          <div className="flex justify-start items-center border-[1px] border-stone-400 w-[430px] h-[37px] rounded-md ml-8 text-stone-400 text-xs font-normal bg-[#1D1D1D]">
-            <IoSearch className="text-stone-400 w-5 h-5 ml-4" />
-            <p className="ml-2 mr-2">
-              Search for specific componants, sections, wireframe, and more...
-            </p>
+        <div className="w-full max-w-[800px] px-4 sm:px-8">
+          <div className="flex justify-start items-center h-[37px] rounded-md text-stone-400 text-xs font-normal bg-[#1D1D1D]">
+            <IoSearch className="text-stone-400 w-5 h-5 ml-2 sm:ml-4" />
+            <input
+              className="ml-2 sm:ml-4 mr-2 bg-transparent outline-none flex-grow text-white"
+              placeholder="Search for specific components, sections, wireframes, and more..."
+              spellCheck="false"
+              type="text"
+            />
           </div>
         </div>
-        <div className=" border-[1px] border-stone-400 rounded-md ml-8 h-[38px] w-[38px] flex items-center justify-center">
-          <FiUser className="text-white w-6 h-6 " />
+
+        <div className="border-[1px] border-[#1D1D1D] rounded-md mx-4 h-[38px] w-[38px] flex items-center justify-center aspect-square">
+          <FiUser className="text-white w-10 h-6" />
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-import Navbar from "./component/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-4 bg-[#292929] ">
+    <main className="border-2 border-[#292929] w-full min-h-screen p-4 bg-[#292929] ">
       <Navbar />
+      <div className=" text-white border-t-[1px] border-[#292929]  w-[280px] h-[923px] gap-24 p-8 bg-[#121212]">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other
+              components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It&apos;s animated by default, but you can disable it if you
+              prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </main>
   );
 }
