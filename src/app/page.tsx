f7bd003c61785a790eff1d93c15cb8bf00356d60
Valtkf <valentin.vgregorio@gmail.com>
2024-08-11 19:10:53 +0200

    💄 F0 : styling side menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Navbar from "./components/navbar/Navbar";
+import { TbBolt } from "react-icons/tb";
+import { GrAppsRounded } from "react-icons/gr";
+import { BsStars } from "react-icons/bs";
+import { IoBookmarkOutline } from "react-icons/io5";
+import { FaRegStar } from "react-icons/fa";
+
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@radix-ui/react-accordion";
+} from "@/components/ui/accordion";
+import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
@@ -13,24 +19,47 @@ export default function Home() {
       <div className=" text-white border-t-[1px] border-[#292929]  w-[280px] h-[923px] gap-24 p-8 bg-[#121212]">
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
+            <AccordionTrigger className="text-sm">
+              <TbBolt size={20} />
+              PLATFORM
+            </AccordionTrigger>
+            <AccordionContent>Weblflow</AccordionContent>
+            <AccordionContent>Figma</AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger>Is it styled?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components&apos; aesthetic.
-            </AccordionContent>
+            <AccordionTrigger className="text-sm">
+              <GrAppsRounded size={20} />
+              SECTIONS
+            </AccordionTrigger>
+            <AccordionContent>Hero</AccordionContent>
+            <AccordionContent>How it Works</AccordionContent>
+            <AccordionContent>Services</AccordionContent>
+            <AccordionContent>Testimonials</AccordionContent>
+            <AccordionContent>Pricing</AccordionContent>
+            <AccordionContent>FAQ</AccordionContent>
+            <AccordionContent>Footer</AccordionContent>
+            <AccordionContent>Show more +</AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
-            <AccordionTrigger>Is it animated?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It&apos;s animated by default, but you can disable it if you
-              prefer.
-            </AccordionContent>
+            <AccordionTrigger className="text-sm">
+              <BsStars size={20} />
+              STYLE
+            </AccordionTrigger>
+            <AccordionContent>Coming soon</AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger className="text-sm">
+              <IoBookmarkOutline size={20} />
+              SAVED
+            </AccordionTrigger>
+            <AccordionContent>Coming soon</AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-5">
+            <AccordionTrigger className="text-sm">
+              <FaRegStar size={20} />
+              PLAN
+            </AccordionTrigger>
+            <AccordionContent>Coming soon</AccordionContent>
           </AccordionItem>
         </Accordion>
       </div>
