fc2fc03cd30ef7d5d83ba98b11a0e4343b7811d9
Valtkf <valentin.vgregorio@gmail.com>
2024-08-11 19:32:22 +0200

    ✨  F0 : Add Sidebar component

---
+++ src/app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,131 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import React from "react";
+import { motion } from "framer-motion";
+import { TbBolt } from "react-icons/tb";
+import { GrAppsRounded } from "react-icons/gr";
+import { BsStars } from "react-icons/bs";
+import { IoBookmarkOutline } from "react-icons/io5";
+import { FaRegStar } from "react-icons/fa";
+import { useState } from "react";
+
+export default function Sidebar() {
+  const [activeItem, setActiveItem] = useState<string | null>(null);
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className="w-full"
+      value={activeItem}
+      onValueChange={setActiveItem}
+    >
+      <AccordionItem value="item-1">
+        <AccordionTrigger
+          className={`text-sm ${activeItem === "item-1" ? "text-white" : ""}`}
+        >
+          <TbBolt size={20} />
+          PLATFORM
+        </AccordionTrigger>
+        <motion.div
+          className="accordion-content"
+          initial={{ height: 0, opacity: 0 }}
+          animate={{
+            height: activeItem === "item-1" ? "auto" : 0,
+            opacity: activeItem === "item-1" ? 1 : 0,
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          <AccordionContent>Webflow</AccordionContent>
+          <AccordionContent>Figma</AccordionContent>
+        </motion.div>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger
+          className={`text-sm ${activeItem === "item-2" ? "text-white" : ""}`}
+        >
+          <GrAppsRounded size={20} />
+          SECTIONS
+        </AccordionTrigger>
+        <motion.div
+          className="accordion-content"
+          initial={{ height: 0, opacity: 0 }}
+          animate={{
+            height: activeItem === "item-2" ? "auto" : 0,
+            opacity: activeItem === "item-2" ? 1 : 0,
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          <AccordionContent>Hero</AccordionContent>
+          <AccordionContent>How it Works</AccordionContent>
+          <AccordionContent>Services</AccordionContent>
+          <AccordionContent>Testimonials</AccordionContent>
+          <AccordionContent>Pricing</AccordionContent>
+          <AccordionContent>FAQ</AccordionContent>
+          <AccordionContent>Footer</AccordionContent>
+          <AccordionContent>Show more +</AccordionContent>
+        </motion.div>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger
+          className={`text-sm ${activeItem === "item-3" ? "text-white" : ""}`}
+        >
+          <BsStars size={20} />
+          STYLE
+        </AccordionTrigger>
+        <motion.div
+          className="accordion-content"
+          initial={{ height: 0, opacity: 0 }}
+          animate={{
+            height: activeItem === "item-3" ? "auto" : 0,
+            opacity: activeItem === "item-3" ? 1 : 0,
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          <AccordionContent>Coming soon</AccordionContent>
+        </motion.div>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger
+          className={`text-sm ${activeItem === "item-4" ? "text-white" : ""}`}
+        >
+          <IoBookmarkOutline size={20} />
+          SAVED
+        </AccordionTrigger>
+        <motion.div
+          className="accordion-content"
+          initial={{ height: 0, opacity: 0 }}
+          animate={{
+            height: activeItem === "item-4" ? "auto" : 0,
+            opacity: activeItem === "item-4" ? 1 : 0,
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          <AccordionContent>Coming soon</AccordionContent>
+        </motion.div>
+      </AccordionItem>
+      <AccordionItem value="item-5">
+        <AccordionTrigger
+          className={`text-sm ${activeItem === "item-5" ? "text-white" : ""}`}
+        >
+          <FaRegStar size={20} />
+          PLAN
+        </AccordionTrigger>
+        <motion.div
+          className="accordion-content"
+          initial={{ height: 0, opacity: 0 }}
+          animate={{
+            height: activeItem === "item-5" ? "auto" : 0,
+            opacity: activeItem === "item-5" ? 1 : 0,
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          <AccordionContent>Coming soon</AccordionContent>
+        </motion.div>
+      </AccordionItem>
+    </Accordion>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,14 @@
-import { TbBolt } from "react-icons/tb";
-import { GrAppsRounded } from "react-icons/gr";
-import { BsStars } from "react-icons/bs";
-import { IoBookmarkOutline } from "react-icons/io5";
-import { FaRegStar } from "react-icons/fa";
+"use client";
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 export default function Home() {
   return (
     <main className="border-2 border-[#292929] w-full min-h-screen p-4 bg-[#292929] ">
       <Navbar />
-      <div className=" text-white border-t-[1px] border-[#292929]  w-[280px] h-[923px] gap-24 p-8 bg-[#121212]">
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="text-sm">
-              <TbBolt size={20} />
-              PLATFORM
-            </AccordionTrigger>
-            <AccordionContent>Weblflow</AccordionContent>
-            <AccordionContent>Figma</AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger className="text-sm">
-              <GrAppsRounded size={20} />
-              SECTIONS
-            </AccordionTrigger>
-            <AccordionContent>Hero</AccordionContent>
-            <AccordionContent>How it Works</AccordionContent>
-            <AccordionContent>Services</AccordionContent>
-            <AccordionContent>Testimonials</AccordionContent>
-            <AccordionContent>Pricing</AccordionContent>
-            <AccordionContent>FAQ</AccordionContent>
-            <AccordionContent>Footer</AccordionContent>
-            <AccordionContent>Show more +</AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger className="text-sm">
-              <BsStars size={20} />
-              STYLE
-            </AccordionTrigger>
-            <AccordionContent>Coming soon</AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-4">
-            <AccordionTrigger className="text-sm">
-              <IoBookmarkOutline size={20} />
-              SAVED
-            </AccordionTrigger>
-            <AccordionContent>Coming soon</AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-5">
-            <AccordionTrigger className="text-sm">
-              <FaRegStar size={20} />
-              PLAN
-            </AccordionTrigger>
-            <AccordionContent>Coming soon</AccordionContent>
-          </AccordionItem>
-        </Accordion>
+      <div className="text-[#7E7F81] border-t-[1px] border-[#292929] w-[280px] h-[923px] gap-24 p-8 bg-[#121212]">
+        <Sidebar />
       </div>
     </main>
   );
