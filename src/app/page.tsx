9c04263ed409d61f7cb02e9d32d42949e28e3b15
Valtkf <valentin.vgregorio@gmail.com>
2024-08-12 17:32:22 +0200

    💄  F0: add layout for feature Hero Section and Other Sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,16 @@ import Sidebar from "./components/sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <main className="border-2 border-[#292929] w-full min-h-screen p-4 bg-[#292929] ">
+    <main className="border-2 border-[#292929] w-full min-h-screen p-4 bg-[#292929]">
       <Navbar />
-      <div className="rounded-bl-3xl text-[#7E7F81] border-t-[1px] border-[#292929] w-[280px] h-[923px] gap-24 p-8 bg-[#121212]">
-        <Sidebar />
-        <div className="absolute top-[900px]">
-          <ButtonOutline />
+      <div className="flex">
+        <div className="rounded-bl-3xl text-[#7E7F81] border-t-[1px] border-[#292929] w-[280px] h-[923px] gap-24 p-8 bg-[#121212] flex-shrink-0">
+          <Sidebar />
+          <div className="absolute top-[900px]">
+            <ButtonOutline />
+          </div>
         </div>
+        <div className="border-2 border-red-400 rounded-br-3xl min-h-screen flex-grow bg-[#121212]"></div>
       </div>
     </main>
   );
